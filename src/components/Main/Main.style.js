50a97c0c4f3ace141c295bbd4dce27c126a45958
Alisher778 <alisher.musurmonov89@gmail.com>
2022-01-13 11:15:18 +0500

    feat: handle completed and important status changes

--- src/components/Main/Main.style.js
+++ src/components/Main/Main.style.js
@@ -5,7 +5,8 @@ import { pxToRem } from '../../utils';
 export const StyledMain = styled.section`
   padding: ${pxToRem(20)};
   width: 100%;
-  
+  height: calc(100vh - ${pxToRem(STYLING_CONFIGS.HEADER_HEIGHT)});
+  overflow: auto;
 `;
 
 
