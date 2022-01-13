87c3ea51bd0a65300efda417ea6c5213e86fb718
Alisher778 <alisher.musurmonov89@gmail.com>
2022-01-13 09:18:38 +0500

    finish documentation

--- src/components/ToDos/ToDos.jsx
+++ src/components/ToDos/ToDos.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { StyledTodos } from './ToDos.style';
 import AddTodo from '../AddToDo';
+import axios from 'axios';
 
 import Mockdata from '../../Mocks/todos';
 import ToDoItem from './ToDoItem';
+import { useEffect } from 'react';
 
 export default function ToDos() {
   const location = useLocation();
