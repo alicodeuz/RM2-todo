b8e03175c9ddfa79620276b5b84f34a488295a69
Alisher778 <alisher.musurmonov89@gmail.com>
2022-01-20 10:15:41 +0500

    feat: add lazy code splitting and sign-in logic

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,42 +1,40 @@
-import React, { useState } from 'react';
+import React, { Suspense, useState, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Header from '../components/Header/Header';
-import Main from '../components/Main';
-import Sidebar from '../components/Sidebar';
 import { StyledMainContenWrapper } from './App.style';
-import SignUp from './Auth/SignUp';
-import SignIn from './Auth/SignuIn';
-import Welcome from './Welcome/Welcome';
+
+const Header = lazy(() => import('../components/Header/Header'));
+const Main = lazy(() => import('../components/Main'));
+const Sidebar = lazy(() => import('../components/Sidebar'));
+const SignUp = lazy(() => import('./Auth/SignUp'));
+const SignIn = lazy(() => import('./Auth/SignIn'));
+const Welcome = lazy(() => import('./Welcome/Welcome'));
 
 export default function App() {
-  const [isRed, setIsRed] = useState(false);
-  const [isLogged, setIsLogged] = useState(false);
-  const [user, setUser] = useState({
-    id: null,
-    username: '',
-    email: ''
-  });
+  const user = JSON.parse(localStorage.user || "{}");
+  const token = localStorage.token;
 
-  if (isLogged) {
+  if (token && user?.id) {
     return (
-      <div>
-        <Header />
-        <StyledMainContenWrapper>
-          <Sidebar />
-          <Main />
-        </StyledMainContenWrapper>
-      </div>
+      <Suspense fallback="Loading...">
+        <div>
+          <Header />
+          <StyledMainContenWrapper>
+            <Sidebar />
+            <Main />
+          </StyledMainContenWrapper>
+        </div>
+      </Suspense>
     )
   }
   return (
     <>
-
-      <Routes>
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="*" element={<Welcome />} />
-      </Routes>
+      <Suspense fallback="Loading...">
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="*" element={<Welcome />} />
+        </Routes>
+      </Suspense>
     </>
-
   )
 }
---
+++ src/pages/Auth/SignIn.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import axios from '../../utils/axios';
+import microsoft from '../../assets/images/microsoft.png';
+import { StyledSignIn } from './Auth.style';
+// import {useHistory} from 'react-router-dom'
+
+function SignIn() {
+  let navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({
+    identifier: '',
+    password: ''
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUser(prevState => ({ ...prevState, [name]: value }));
+  }
+
+  const handleSubmit = async () => {
+    try {
+      const { password, identifier } = user;
+      if (password && identifier) {
+        setLoading(true);
+        const { data } = await axios.post('/auth/local', user);
+        console.log(data);
+        Swal.fire({
+          icon: 'success',
+          title: 'Success',
+          text: 'You have successfully signed in',
+          footer: `<a href="/">Home page</a>`
+        }).then((value) => {
+          setLoading(false);
+          localStorage.setItem('user', JSON.stringify(data.user))
+          localStorage.setItem('token', data.jwt);
+          window.location.href = '/';
+        })
+
+      } else {
+        Swal.fire({
+          icon: 'error',
+          text: 'Please fill all required fields',
+        });
+      }
+
+    } catch (error) {
+      console.log(error.response.data)
+      Swal.fire({
+        icon: 'error',
+        text: error.response?.data?.error?.message || 'Something went wrong',
+      });
+      setLoading(false);
+    }
+  }
+  const { password, identifier } = user;
+
+  return (
+    <StyledSignIn>
+      <div className="signin">
+        <div className='sign-in__header'>
+          <img src={microsoft} alt="microsoft" onClick={() => navigate('/')} />
+          <h2>Sign up</h2>
+        </div>
+        <div className="sign-in__body">
+          <div className='sign-in__input-wrapper'>
+            {/* <label htmlFor="">Username/Email</label> */}
+            <input
+              type="text"
+              name="identifier"
+              value={identifier}
+              placeholder='Email/Username'
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className='sign-in__input-wrapper'>
+            {/* <label htmlFor="">Password</label> */}
+            <input
+              type="text"
+              name="password"
+              value={password}
+              placeholder='Password'
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="sign-in__footer">
+          <p>Not have an account? <Link to="/sign-up">Sign up!</Link> </p>
+        </div>
+        <div className="signin__actions">
+          <button onClick={handleSubmit} disabled={loading}>
+            {loading ? 'Signing in...' : 'Sign In'}
+          </button>
+        </div>
+      </div>
+    </StyledSignIn>
+
+  )
+}
+
+export default SignIn;
